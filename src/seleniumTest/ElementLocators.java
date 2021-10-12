fb694afa9d2593547d4f6737fda7c0d56bfe6932
Bakry13 <mohamed.bakry13@gmail.com>
2021-10-13 01:32:38 +0200

    Element locators and actions

--- src/seleniumTest/ElementLocators.java
+++ src/seleniumTest/ElementLocators.java
@@ -9,26 +9,16 @@ import org.openqa.selenium.interactions.Actions;
 public class ElementLocators {
 	
 	ChromeDriver driver;
-	Actions actions;
 	void invokedbrowser()
 	{
 		try {
 			System.setProperty("webdriver.chrome.driver",
-			"C:\\MyData\\Courses\\CourseProjects\\SeleniumFirstProject\\src\\resources\\chromedriver.exe");
+			"C:\\Users\\Mohamed\\Desktop\\Course\\SeleniumFirstProject\\src\\resources\\chromedriver.exe");
 			driver = new ChromeDriver();
 			driver.get("http://automationpractice.com/");
+//			driver.get("https://www.google.com.eg/");
 			driver.manage().window().maximize();
 			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-//			driver.findElement(By.xpath("//a[contains(text(),'Sign in')]")).click();
-//			driver.findElement(By.id("email")).sendKeys("test@moakt.cc");
-//			driver.findElement(By.id("passwd")).sendKeys("Test@123");
-//			driver.findElement(By.name("SubmitLogin")).click();
-//			driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/header[1]/div[3]/div[1]/div[1]/div[2]/form[1]/input[4]")).sendKeys("Blouse");
-//			driver.findElement(By.cssSelector("button[name='submit_search']")).click();
-			Thread.sleep(1000);
-			actions.moveToElement(driver.findElement(By.linkText("Popular"))).build().perform();
-			driver.findElement(By.linkText("Popular")).click();
-//			driver.quit();
 			} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -41,3 +31,34 @@ public class ElementLocators {
 	}
 
 }
+
+
+
+
+
+
+
+
+//=======================Element locators===================
+//driver.findElement(By.xpath("//a[contains(text(),'Sign in')]")).click();
+//driver.findElement(By.id("email")).sendKeys("test@moakt.cc");
+//driver.findElement(By.id("passwd")).sendKeys("Test@123");
+//driver.findElement(By.name("SubmitLogin")).click();
+//driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/header[1]/div[3]/div[1]/div[1]/div[2]/form[1]/input[4]")).sendKeys("Blouse");
+//driver.findElement(By.cssSelector("button[name='submit_search']")).click();
+//driver.findElement(By.linkText("Gmail")).click();
+//driver.findElement(By.partialLinkText("Gma")).click();
+//=================Get commands================
+//String title = driver.getTitle();
+//String url = driver.getCurrentUrl();
+//String sourceCode = driver.getPageSource();
+//System.out.println(title);
+//System.out.println(url);
+//System.out.println(sourceCode);
+//driver.navigate().to("https://www.google.com.eg/");
+//driver.navigate().refresh();
+//driver.navigate().back();
+//driver.navigate().forward();
+//================Close - Quit==============
+//driver.close();
+//driver.quit();
