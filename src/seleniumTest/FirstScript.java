ff4638fdfa77e36a0778f0c5c9cf0a1b78e062fd
Bakry13 <mohamed.bakry13@gmail.com>
2023-11-20 23:46:43 +0200

    update to selenium 4 external jars

--- src/seleniumTest/FirstScript.java
+++ src/seleniumTest/FirstScript.java
@@ -1,23 +1,23 @@
 package seleniumTest;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class FirstScript {
 	WebDriver driver;
 	void invokedbrowser()
 	{
 		try {
-			System.setProperty("webdriver.chrome.driver",
-			"C:\\Users\\Mohamed\\Desktop\\Course\\SeleniumFirstProject\\src\\resources\\chromedriver.exe");
+//			System.setProperty("webdriver.chrome.driver",
+//			"C:\\Users\\Mohamed\\Desktop\\Course\\SeleniumFirstProject\\src\\resources\\chromedriver.exe");
 			driver = new ChromeDriver();
 			//============================================================================
 			driver.get("http://automationpractice.com/");
 			driver.manage().window().maximize();
-			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
 			driver.findElement(By.xpath("//a[contains(text(),'Sign in')]")).click();
 			driver.findElement(By.id("email")).sendKeys("test@moakt.cc");
 			driver.findElement(By.id("passwd")).sendKeys("Test@123");
